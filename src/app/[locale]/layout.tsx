17b524a45b42f8bdb4dc539da499efd01227afae
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2024-08-06 15:46:07 +0700

    chore: merge 2 layouts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,17 @@
+import Navbar from '@/components/Navbar/navbar';
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
 
 export default async function LocaleLayout({ children }: { children: React.ReactNode }) {
   const messages = await getMessages();
 
-  return <NextIntlClientProvider messages={messages}>{children}</NextIntlClientProvider>;
+  return (
+    <NextIntlClientProvider messages={messages}>
+      <div className='fixed top-0 z-50 w-full bg-[#0D0D0D]'>
+        <Navbar />
+      </div>
+      <div className='relative pb-9 pt-24'>{children}</div>
+      {/* <Footer /> */}
+    </NextIntlClientProvider>
+  );
 }
