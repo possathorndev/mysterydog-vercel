3f13094bbda1833480faa69362f6f93a1b413331
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2024-08-07 15:56:41 +0700

    chore: map pin

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import Footer from '@/components/Footer/Footer';
+import MapPin from '@/components/MapPin/MapPin';
 
 export default async function MainLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,6 +8,7 @@ export default async function MainLayout({ children }: { children: React.ReactNo
       <div className='w-full'>
         <Footer />
       </div>
+      <MapPin />
     </>
   );
 }
