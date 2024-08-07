ce6e016ab349972b8dad88321b1aa2f143e8091c
Kantapong Chanthanawan <yeem.kantapong@gmail.com>
2024-08-07 11:01:36 +0700

    chore: add desktop navbar

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,17 @@
 export const locales = ['en', 'th'];
 export const defaultLocale = 'en';
+
+export const NavbarMenuList = [
+  {
+    title: 'Area List',
+    url: '/locations',
+  },
+  {
+    title: 'Map',
+    url: '/maps',
+  },
+  {
+    title: 'Blog',
+    url: '/blogs',
+  },
+];
