03a4ea7719d2c54530d6d67d8c9d6b87ccd71b2c
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-09 17:04:40 +0700

    chore: allow image from aws s3

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/utils/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mystery-dog.s3.ap-southeast-1.amazonaws.com',
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
