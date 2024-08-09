224a1dce9690e8ae39a02e62a6ef3a5f89147e55
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-09 17:08:11 +0700

    chore: category list component

---
+++ src/components/Category/CategoryList.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import CategoryThumbnailCard from '@/components/Category/CategoryThumbnailCard';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { Category } from '@/lib/api/categories';
+import { useTranslations } from 'next-intl';
+
+const CategoryList = ({ data }: { data?: Category[] }) => {
+  const t = useTranslations('Global');
+
+  return (
+    <div className='my-4'>
+      <div className='text-2xl font-bold text-main'>{t('category')}</div>
+      <div className='font-gaegu text-lg font-bold text-secondary'>{t('categoryDescription')}</div>
+      <ScrollArea className='w-[calc(100vw-32px)] whitespace-nowrap'>
+        <div className='mt-4 flex gap-2'>
+          {data?.map((item, index) => <CategoryThumbnailCard key={index} data={item} />)}
+        </div>
+        <ScrollBar orientation='horizontal' />
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default CategoryList;
