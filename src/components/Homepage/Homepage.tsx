236c7b4ceaef9d48cd9b06dea669b07b059dda1f
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-12 10:06:52 +0700

    chore: add image above open map

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -13,8 +13,12 @@ export default function Homepage({ initialData }: { initialData?: Home }) {
     <div className='mx-auto flex w-full flex-col gap-4'>
       <BannerImages images={data?.bannerImages} />
       <div className='mx-auto flex max-w-screen-2xl flex-col gap-6 px-2 md:gap-8 md:px-6 xl:px-0'>
-        <CategoryList data={data?.categories?.data?.map((item) => item.attributes)} />
-        <MapBanner />
+        {/* Reverse order when desktop */}
+        <div className='flex flex-col gap-6 md:flex-col-reverse'>
+          <MapBanner />
+          <CategoryList data={data?.categories?.data?.map((item) => item.attributes)} />
+        </div>
+
         {/* <LocationNearMe /> */}
         {/* <BlogList /> */}
         {/* <AreaList /> */}
--- src/components/Homepage/MapBanner.tsx
+++ src/components/Homepage/MapBanner.tsx
@@ -10,7 +10,7 @@ const MapBanner = () => {
   const t = useTranslations('HomePage');
   return (
     <div
-      className='relative h-80 w-full rounded-md border-2 bg-primary/10 md:h-56'
+      className='relative h-56 w-full rounded-md border-2 bg-primary/10'
       style={{
         backgroundImage: 'url(/images/maps.png)',
         backgroundPosition: 'center',
@@ -20,12 +20,15 @@ const MapBanner = () => {
       }}
     >
       <div className='flex h-full flex-col justify-between md:flex-row'>
-        <div className='flex h-full w-full flex-col justify-end gap-2 px-4 py-2 md:w-1/3 md:p-6 md:px-4 md:py-4'>
+        <div className='hidden h-full w-full flex-col justify-end gap-2 px-4 py-2 md:flex md:w-1/3 md:p-6 md:px-4 md:py-4'>
           <div className='text-2xl font-bold text-font-header'>{t('mapTitle')}</div>
           <div className='font-gaegu text-xl text-font-description'>{t('mapDescription')}</div>
         </div>
         <div className='flex h-full w-full flex-col justify-end px-4 py-2 md:w-1/3 md:p-6 md:px-4 md:py-4'>
-          <div className='flex h-28 w-full justify-between rounded-md bg-white p-4'>
+          <div className='relative flex h-28 w-full justify-between rounded-md bg-white p-4'>
+            <div className='absolute -top-[75px] left-4 block md:hidden'>
+              <Image src='/images/french-bulldog.png' alt='French Bulldog' width={85} height={75} />
+            </div>
             <div className='flex w-2/3 flex-col gap-2'>
               <div className='text-2xl font-bold text-primary'>{t('mapLinkTitle')}</div>
               <div className='font-gaegu text-base font-light leading-4 text-secondary'>{t('mapLinkDescription')}</div>
@@ -34,7 +37,7 @@ const MapBanner = () => {
               className='ml-4 flex w-1/3 flex-col items-center justify-center gap-2 rounded-md bg-secondary/10 p-0'
               href={MAPS_PATH}
             >
-              <Image src='/icons/arrow-up-right.png' alt='Map Pin' width={18} height={18} className='' />
+              <Image src='/icons/arrow-up-right.png' alt='Map Pin' width={18} height={18} />
               <div className='text-center text-sm font-bold text-secondary'>{t('mapLinkNavigate')}</div>
             </Link>
           </div>
