7965da834a9a5359f236214ff33a2b77b3e758ae
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-22 10:37:42 +0700

    chore: locale URL only for TH

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,27 @@
 import createMiddleware from 'next-intl/middleware';
-
 import { auth } from '@/auth';
-import { locales, defaultLocale } from '@/constants/config';
+import { locales, defaultLocale, localePrefix } from '@/constants/config';
 
 const intlMiddleware = createMiddleware({
   locales,
   defaultLocale,
-  localePrefix: 'never',
+  localePrefix,
 });
+
+export const config = {
+  matcher: [
+    // Enable a redirect to a matching locale at the root
+    '/',
+
+    // Set a cookie to remember the previous locale for
+    // all requests that have a locale prefix
+    '/(th)/:path*', // (th|en) for both locales
+
+    // Enable redirects that add missing locales
+    // (e.g. `/pathnames` -> `/en/pathnames`)
+    '/((?!_next|_vercel|.*\\..*).*)',
+  ],
+};
 
 export default auth((request) => {
   const { pathname } = request.nextUrl;
