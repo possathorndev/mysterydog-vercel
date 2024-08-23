f0b9bea10868d2be0c2645a4cff626430105a32d
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-23 15:17:56 +0700

    fix: build failed to maps

--- src/app/[locale]/(map)/layout.tsx
+++ src/app/[locale]/(map)/layout.tsx
@@ -1,16 +1,19 @@
 import { MapParamsContextProvider } from '@/contexts/MapProvider/MapParamsProvider';
 import { MapSheetContextProvider } from '@/contexts/MapProvider/MapSheetProvider';
 import { MapFormContextProvider } from '@/contexts/MapProvider/MapFormProvider';
+import { Suspense } from 'react';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   // return <div className='relative pt-[70px]'>{children}</div>;
   return (
-    <MapParamsContextProvider>
-      <MapFormContextProvider>
-        <MapSheetContextProvider>
-          <div className='relative'>{children}</div>
-        </MapSheetContextProvider>
-      </MapFormContextProvider>
-    </MapParamsContextProvider>
+    <Suspense fallback={null}>
+      <MapParamsContextProvider>
+        <MapFormContextProvider>
+          <MapSheetContextProvider>
+            <div className='relative'>{children}</div>
+          </MapSheetContextProvider>
+        </MapFormContextProvider>
+      </MapParamsContextProvider>
+    </Suspense>
   );
 }
