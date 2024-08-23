d79295654461176d9e6fe00217c673d3c2da4809
Kantapong Chanthanawan <yeem@privatus.tech>
2024-08-23 11:40:15 +0700

    chore: try new middle with next-auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
 import { auth } from '@/auth';
 import { locales, defaultLocale, localePrefix } from '@/constants/config';
+import { NextRequest } from 'next/server';
+
+const publicPages = [
+  '/',
+  '/login',
+  // (/secret requires auth)
+];
 
 const intlMiddleware = createMiddleware({
   locales,
@@ -9,21 +16,47 @@ const intlMiddleware = createMiddleware({
   localeDetection: false,
 });
 
+const authMiddleware = auth(
+  // Note that this callback is only invoked if
+  // the `authorized` callback has returned `true`
+  // and not for pages listed in `pages`.
+  (req) => intlMiddleware(req),
+);
+
+export default function middleware(req: NextRequest) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${publicPages.flatMap((p) => (p === '/' ? ['', '/'] : p)).join('|')})/?$`,
+    'i',
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
+}
+
 export const config = {
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(th/en)/:path*', // (th|en) for both locales
-
-    // Enable redirects that add missing locales
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-  ],
+  // Skip all paths that should not be internationalized
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
 
-export default auth((request) => {
-  return intlMiddleware(request);
-});
+// export const config = {
+//   matcher: [
+//     // Enable a redirect to a matching locale at the root
+//     '/',
+
+//     // Set a cookie to remember the previous locale for
+//     // all requests that have a locale prefix
+//     '/(th/en)/:path*', // (th|en) for both locales
+
+//     // Enable redirects that add missing locales
+//     // (e.g. `/pathnames` -> `/en/pathnames`)
+//     '/((?!api|_next|_vercel|.*\\..*).*)',
+//   ],
+// };
+
+// export default auth((request) => {
+//   return intlMiddleware(request);
+// });
