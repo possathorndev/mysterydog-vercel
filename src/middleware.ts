30aeee6564f7c52f2d07bc3f171e8ccf2f905817
Billy <wpossathorn@gmail.com>
2024-09-04 08:14:48 +0700

    fix: test middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,29 +37,29 @@ const intlMiddleware = createIntlMiddleware({
 //   }
 // });
 
-const handleAuthMiddleware = async (req: NextRequest, isPrivatePage: boolean, isAuthPage: boolean) => {
-  const { nextUrl } = req;
-
-  const session = await auth();
-  const isLoggedIn = !!session?.user;
-
-  if (!isLoggedIn && isPrivatePage) return NextResponse.redirect(new URL('/auth/login', nextUrl));
-  if (isLoggedIn && isAuthPage) return NextResponse.redirect(new URL('/', nextUrl));
-
-  return intlMiddleware(req);
-};
+// const handleAuthMiddleware = async (req: NextRequest, isPrivatePage: boolean, isAuthPage: boolean) => {
+//   const { nextUrl } = req;
+//
+//   const session = await auth();
+//   const isLoggedIn = !!session?.user;
+//
+//   if (!isLoggedIn && isPrivatePage) return NextResponse.redirect(new URL('/auth/login', nextUrl));
+//   if (isLoggedIn && isAuthPage) return NextResponse.redirect(new URL('/', nextUrl));
+//
+//   return intlMiddleware(req);
+// };
 
 export default function middleware(req: NextRequest) {
   const isPrivatePage = testPathnameRegex(privatePages, req.nextUrl.pathname);
   const isAuthPage = testPathnameRegex(authPages, req.nextUrl.pathname);
 
-  return handleAuthMiddleware(req, isPrivatePage, isAuthPage);
+  // return handleAuthMiddleware(req, isPrivatePage, isAuthPage);
 
   // if (isAuthPage || isPrivatePage) {
   //   return (authMiddleware as any)(req);
   // }
   //
-  // return intlMiddleware(req);
+  return intlMiddleware(req);
 }
 
 export const config = {
