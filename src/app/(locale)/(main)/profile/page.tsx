2ce79ec17093271a682603fe31ddee6e6fbc90bd
Billy <wpossathorn@gmail.com>
2024-09-04 10:32:16 +0700

    fix: remove next-intl

--- src/app/(locale)/(main)/profile/page.tsx
+++ src/app/(locale)/(main)/profile/page.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 // Components
 import ProfileInfo from '@/components/Profile/ProfileInfo';
+import AuthenticationModal from '@/components/Navbar/RightNavbar/AuthenticationModal';
 
 export default async function Profile({ params }: { params: { locale: string } }) {
-  return <ProfileInfo />;
+  return (
+    <div>
+      <ProfileInfo />
+      <AuthenticationModal />
+    </div>
+  );
 }
